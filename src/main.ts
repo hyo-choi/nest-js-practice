1798b531c7cc7a608251b6193bdb1511bc9d037b
hyo-choi <hyochoi31@gmail.com>
2021-10-20 20:44:56 +0900

    Feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API document')
+    .setDescription('NestJS practice document')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // https://docs.nestjs.com/openapi/introduction
+
   await app.listen(3000);
 }
 bootstrap();
